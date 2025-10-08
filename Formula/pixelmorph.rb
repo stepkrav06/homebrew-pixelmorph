38a2250b684c38cb6d2c5214caaa21f6c0307783
Stepan Kravtsov <stepankravtsov06@gmail.com>
2025-10-07 22:46:24 -0400

    formula fixes

--- Formula/pixelmorph.rb
+++ Formula/pixelmorph.rb
@@ -5,7 +5,7 @@ class Pixelmorph < Formula
   desc "Transform and animate image morphs using optimization algorithms"
   homepage "https://github.com/stepkrav06/pixelmorph"
   url "https://github.com/stepkrav06/pixelmorph/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7bd5abc3f0c0c46645836316bd2827b20684c3d8f97c9ab93444af7c543ad07d"
+  sha256 "f6f56dbb09231c235e9ab6da39292d5960ef21320f7c664370423e990e8e08e2"
   license "MIT"
 
   depends_on "python@3.11"
